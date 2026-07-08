50f810ba97feaf1f0cc79b9adc52f19d232e3358
voidmahipal <mahipal7131@gmail.com>
2026-07-08 19:58:51 +0530

    Added randomized color allocation to sqaures when hovered over

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,3 +1,18 @@
+function getARandomNumber(min,max) {
+    return Math.floor(Math.random()*(max-min)+min);
+}
+const colors = [
+  "#4F86F7", "#5DADE2", "#4DD0E1", "#26A69A", "#2ECC71",
+  "#66BB6A", "#8BC34A", "#9CCC65", "#F4D03F", "#F5B041",
+  "#F39C12", "#FF7F50", "#E74C3C", "#EC7063", "#EC87C0",
+  "#D252B9", "#9B59B6", "#6C5CE7", "#A97142", "#78909C"
+];
+const lightColors = [
+  "#AFC8FC", "#AED6F1", "#A9E5EB", "#7DCEC4", "#82E0AA",
+  "#A9DFBF", "#C5E1A5", "#D4E6B5", "#F9E79F", "#F8C471",
+  "#F8C471", "#FFB899", "#F1948A", "#F5B7B1", "#F5B7D8",
+  "#E8A2D8", "#C39BD3", "#B2A4F5", "#CFA37A", "#B0BEC5"
+];
 const grid = document.querySelector("div");
 const btn = document.querySelector("button");
 
@@ -11,12 +26,22 @@ function fillgrid(n) {
             col.style.border="1px solid black";
             row.appendChild(col);
             col.style.flex="1";
+
+            let color_idx;
+            col.addEventListener("mouseenter",()=>{
+                if(col.style.backgroundColor!=="") return;
+                color_idx=getARandomNumber(0,20);
+                col.style.backgroundColor=colors[color_idx];
+            })
+            col.addEventListener("mouseleave",()=>{
+                col.style.backgroundColor=lightColors[color_idx];
+            })
         }
         row.style.flex="1";
         grid.appendChild(row);
     }
 }
-fillgrid(10);
+fillgrid(16);
 
 btn.addEventListener("click",()=>{
     let input = prompt("Enter the number of squares per side : ");
@@ -31,6 +56,6 @@ btn.addEventListener("click",()=>{
             side = Number(input);
         }
         grid.replaceChildren();
-        fillgrid(input);
+        fillgrid(side);
     }
 })
